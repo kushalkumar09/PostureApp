09a0da781eb35505690d1bcfea1c88713cffaacd
Kushal Kumar Banyal <kushalbanyal404@gmail.com>
2025-06-12 15:44:19 +0530

    handled oboarding for new user

--- App.jsx
+++ App.jsx
@@ -1,38 +1,63 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { StyleSheet} from 'react-native'
+import React, {useState, useEffect} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {StyleSheet, ActivityIndicator, View} from 'react-native';
+import WelcomeScreen from './src/screens/Onboarding/WelcomeScreen';
+import {storage} from './storage'; 
 
 const Stack = createNativeStackNavigator();
+
 const App = () => {
+  const [firstTimeUser, setFirstTimeUser] = useState(null); 
+
+  useEffect(() => {
+    const storedValue = storage.getString('firstTimeUser');
+    setFirstTimeUser(storedValue !== 'false');
+  }, []);
+
+  const onCompleteOnboarding = () => {
+    storage.set('firstTimeUser', 'false');
+    setFirstTimeUser(false);
+  };
+
+  if (firstTimeUser === null) {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Welcome'>
+      <Stack.Navigator initialRouteName={firstTimeUser ? 'Welcome' : 'PostureMonitoring'}>
         <Stack.Screen
-          name='Welcome'
-          component={require('./src/screens/Onboarding/WelcomeScreen').default}
-          options={{ headerShown: false }}
+          name="Welcome"
+          children={(props) => (
+            <WelcomeScreen {...props} onComplete={onCompleteOnboarding} />
+          )}
+          options={{headerShown: false}}
         />
         <Stack.Screen
-          name='CameraPermission'
+          name="CameraPermission"
           component={require('./src/screens/Onboarding/CameraPermissionScreen').default}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
-          name='HowItWorks'
+          name="HowItWorks"
           component={require('./src/screens/Onboarding/HowItWorksScreen').default}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
         <Stack.Screen
-          name='PostureMonitoring'
+          name="PostureMonitoring"
           component={require('./src/screens/PostureMonitoringScreen').default}
-          options={{ headerShown: false }}
+          options={{headerShown: false}}
         />
       </Stack.Navigator>
     </NavigationContainer>
-    
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
